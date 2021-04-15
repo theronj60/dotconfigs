8f0508420c12c55d71fd28978bcb50f470d9cb4f
Theron Joe <theronj@joetwebdev.io>
2021-04-15 09:54:30 -0600

    create dotconfigs, add .zshrc and alias.zsh. updated author.

---
+++ alias.zsh
@@ -0,0 +1,7 @@
+alias restzsh="source ~/.zshrc"
+alias home="cd ~"
+alias customzsh="cd ~/.oh-my-zsh/custom"
+alias nrd="npm run dev"
+alias nrw="npm run watch"
+alias cl="clear"
+alias bigsur="xcode-select --install"
