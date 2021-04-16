c6fb9674b453b0bc36147cb40496d4eac3a98ea0
Theron Joe <theronj@joetwebdev.io>
2021-04-16 11:26:35 -0600

    updated dot config files, added vimrc.

--- .zshrc
+++ .zshrc
@@ -23,7 +23,7 @@ export TYPEWRITTEN_COLORS="arrow:#266b85"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git laravel web-search z)
+plugins=(git laravel web-search zsh-syntax-highlighting z)
 
 source $ZSH/oh-my-zsh.sh
 
--- alias.zsh
+++ alias.zsh
@@ -1,8 +1,19 @@
-alias restzsh="source ~/.zshrc"
-alias home="cd ~"
+# zsh
+alias reloadzsh="source ~/.zshrc && echo 'zsh refreshed!'"
 alias customzsh="cd ~/.oh-my-zsh/custom"
+alias zshconfig="vim ~/.zshrc"
+alias vimconfig="vim ~/.vimrc" 
+alias zshalias="vim ~/dotconfigs/alias.zsh"
+
+# system
+alias home="cd ~"
+alias cl="clear"
+alias la="ls -a"
+alias bigsur="xcode-select --install"
+
+# dev commands
 alias nrd="npm run dev"
 alias nrw="npm run watch"
-alias cl="clear"
-alias bigsur="xcode-select --install"
-# testing
+
+# git
+alias undocommit="git reset HEAD~1"
