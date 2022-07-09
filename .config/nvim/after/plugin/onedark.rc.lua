c7f827a9efc98edd873417c6f22a249eebc5d32a
Theron Joe <theronj@joetwebdev.io>
2022-07-08 19:58:33 -0600

    setting up alacritty, started removing some commented sections that I don need. change theme style and a couple variables. updated readme.

--- .config/nvim/after/plugin/onedark.rc.lua
+++ .config/nvim/after/plugin/onedark.rc.lua
@@ -1,14 +1,14 @@
 -- Lua
 require('onedark').setup  {
     -- Main options --
-    style = 'cool', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+    style = 'darker', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
     transparent = true,  -- Show/hide background
     term_colors = false, -- Change terminal color as per the selected theme style
     ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
     cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
     -- toggle theme style ---
-    toggle_style_key = '<leader>p', -- Default keybinding to toggle
-    toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+    -- toggle_style_key = '<leader>p', -- Default keybinding to toggle
+    -- toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
 
     -- Change code style ---
     -- Options are italic, bold, underline, none
@@ -26,8 +26,8 @@ require('onedark').setup  {
     highlights = {
 		-- TSKeyword = {fg = '$red', sp = '#0000ff'},
 		-- TSProperty = {fg = '$red', sp = '#0000ff'},
-		TSVariable = {fg = '#E4E4E4', sp = '#0000ff'},
-		TSOperator = {fg = '#E4E4E4', sp = '#0000ff'},
+		TSVariable = {fg = '#D9D9D9', sp = '#0000ff'},
+		TSOperator = {fg = '#D9D9D9', sp = '#0000ff'},
 	}, -- Override highlight groups
 
     -- Plugins Config --
--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # Personal dot Files
 
+## Alacritty
+
+Made the switch to Alacritty from Iterm2
+[Alacritty](https://alacritty.org/)
+Used the default [config](https://github.com/alacritty/alacritty/blob/master/alacritty.yml) example to get started, just uncomment the sections you need and setup.
+
 ## Oh-My-Zsh
-I'm using [Iterm2](https://iterm2.com/) with [Oh-my-zsh](https://ohmyz.sh/)
+[Oh-my-zsh](https://ohmyz.sh/) paired with Alacritty
 
 ## Alias
 alias.zsh is usually found or created in the oh-my-zsh custom folder if you
 ``cd ~/.oh-my-zsh/custom`` then ``touch alias.zsh`` to create it, if it doesn't exist.
 
 ## Neovim
-I made the switch from Vim to [Neovim](https://neovim.io) <br>
+PDE - [Neovim](https://neovim.io) <br>
 Neovim was installed through [Homebrew](https://brew.sh/) with ``brew install neovim`` <br>
 The package manager being used is [vim-plug](https://github.com/junegunn/vim-plug)
 
 ## Tmux
-I have started using tmux. Plugins and settings found in [tmux-config](https://github.com/theronj60/dotconfigs/blob/master/.tmux.conf)
+Plugins and settings found in [tmux-config](https://github.com/theronj60/dotconfigs/blob/master/.tmux.conf)
 
