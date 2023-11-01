738e87cd160bd47d7319860edbec430f46e123c8
Theron Joe <theronj@joetwebdev.io>
2023-11-01 09:37:32 -0600

    added a blade treesitter parser for nvim.

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -36,6 +36,16 @@ require 'nvim-treesitter.configs'.setup {
 	additional_vim_regex_highlighting = false,
 }
 
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.blade = {
+  install_info = {
+    url = "https://github.com/EmranMR/tree-sitter-blade",
+    files = {"src/parser.c"},
+    branch = "main",
+  },
+  filetype = "blade"
+}
+
 vim.treesitter.language.register('html', 'blade') -- the someft filetype will use the python parser and queries.
 -- local ft_to_parser = require"nvim-treesitter.parsers".filetype_to_parsername
 -- ft_to_parser.blade = "html"
